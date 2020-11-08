e947345a7187d60550e9f4973c682907184c622f
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-11-08 10:22:08 +0700

    Index.js setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors'
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb",extend:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extend:true}));
+app.use(cors());
