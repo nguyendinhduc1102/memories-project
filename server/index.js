4574ac2024905b39dabfec814b11de57056572db
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-11-08 10:31:31 +0700

    Setup and Connecting mongodb

--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json({limit:"30mb",extend:true}));
-app.use(bodyParser.urlencoded({limit:"30mb",extend:true}));
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+//mongodb setup
+const CONNECT_URL = 'mongodb+srv://ndd:dinhduc1999@cluster0.znivh.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECT_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
+.catch((error)=>console.error(error.message));
+mongoose.set('useFindAndModify',false);
